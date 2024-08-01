ab3654aff002125d39bb2c8f7ae34ee8e3e04404
Kalita Trevisan <kalitatrevi@icloud.com>
2024-07-31 21:34:27 -0300

    ajustes api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,67 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { CardComponent } from './components/card/card.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CommonModule } from '@angular/common';
+
+interface Post {
+  title: string;
+  thumbnail: string;
+  data: string;
+  author: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CardComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    CardComponent,
+    FooterComponent,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-blog';
+
+  posts: Post[] = [
+    {
+      title: 'Post 1',
+      thumbnail: 'https://via.placeholder.com/300x150',
+      data: '19/09/2022',
+      author: 'Author 1',
+    },
+    {
+      title: 'Post 2',
+      thumbnail: 'https://via.placeholder.com/300x200',
+      data: '19/09/2022',
+      author: 'Author 2',
+    },
+    {
+      title: 'Post 3',
+      thumbnail: 'https://via.placeholder.com/300x100',
+      data: '19/09/2022',
+      author: 'Author 3',
+    },
+    {
+      title: 'Post 4',
+      thumbnail: 'https://via.placeholder.com/300x250',
+      data: '19/09/2022',
+      author: 'Author 4',
+    },
+    {
+      title: 'Post 5',
+      thumbnail: 'https://via.placeholder.com/300x300',
+      data: '19/09/2022',
+      author: 'Author 5',
+    },
+    {
+      title: 'Post 6',
+      thumbnail: 'https://via.placeholder.com/300x150',
+      data: '19/09/2022',
+      author: 'Author 6',
+    },
+  ];
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
-  title = 'card';
+  @Input() title = '';
+  @Input() data = '';
+  @Input() thumbnail = '';
+  @Input() author = '';
 }
